0a14c7289c1397b3a9b7bcd8333f9ddc46266af3
Diegor23 <diego.tamuk@gmail.com>
2020-10-24 12:06:21 -0500

    actually added firebase connectivity

--- shoplocal/php/index.php
+++ shoplocal/php/index.php
@@ -5,7 +5,6 @@
     <title>Shop Local</title>
     <link rel="stylesheet" href="../css/master.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
   </head>
   <body>
 
@@ -26,13 +25,28 @@
     </div>
 
     <!-- The core Firebase JS SDK is always required and must be listed first -->
-    <script src="/__/firebase/7.24.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
 
     <!-- TODO: Add SDKs for Firebase products that you want to use
-    https://firebase.google.com/docs/web/setup#available-libraries -->
-    <script src="/__/firebase/7.24.0/firebase-analytics.js"></script>
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+     <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-analytics.js"></script>
 
-    <!-- Initialize Firebase -->
-    <script src="/__/firebase/init.js"></script>
+     <script>
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        var firebaseConfig = {
+          apiKey: "AIzaSyClkGyBgCWivGSoFt4iPkiU9ArZJJRKJAI",
+          authDomain: "shoplocal-bc619.firebaseapp.com",
+          databaseURL: "https://shoplocal-bc619.firebaseio.com",
+          projectId: "shoplocal-bc619",
+          storageBucket: "shoplocal-bc619.appspot.com",
+          messagingSenderId: "481482497274",
+          appId: "1:481482497274:web:83718e707b38bde726bd43",
+          measurementId: "G-49M9JRCQYV"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+      </script>
   </body>
 </html>
