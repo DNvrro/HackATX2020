2c460def68a5a2efbd170ebfcf017a1a9f97522c
Diegor23 <diego.tamuk@gmail.com>
2020-10-24 09:36:24 -0500

    added website files
    added folder containing folders for website files: folders include php, js, images.

---
+++ shoplocal/js/index.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCPN_ncYzH12UXSdnnuDQnHhu44rcsoq6U",
+    authDomain: "shoplocal-fa326.firebaseapp.com",
+    databaseURL: "https://shoplocal-fa326.firebaseio.com",
+    projectId: "shoplocal-fa326",
+    storageBucket: "shoplocal-fa326.appspot.com",
+    messagingSenderId: "769994140785",
+    appId: "1:769994140785:web:2d3c88d0a670a3f45fbfd0"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ shoplocal/php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Shop Local</title>
+    <link rel="stylesheet" href="../css/master.css">
+    <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
+  </head>
+  <body>
+
+    <div class="signup-image">
+        <img src="../images/shoplocallogo.png" alt="shop local logo">
+    </div>
+
+    <h1>Text text</h1>
+  </body>
+</html>
