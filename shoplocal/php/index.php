e853671bb909a7fc4ddb0df4ba1e8e53bf36e8e9
charliearvizu <charliearvizu@gmail.com>
2020-10-24 11:18:13 -0500

    test 1

--- shoplocal/php/index.php
+++ shoplocal/php/index.php
@@ -15,12 +15,18 @@
 
     <div class="search-container">
        <form action="/action_page.php">
-         <div class="search-field">
-           <input type="text" placeholder="Find..." name="find">
-           <input type="text" placeholder="Near..." name="near">
-           <button type="submit"><i class="fa fa-search"></i></button>
-         </div>
+         <input type="text" placeholder="Search..." name="search">
+         <button type="submit"><i class="fa fa-search"></i></button>
        </form>
     </div>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.24.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.24.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
   </body>
 </html>
