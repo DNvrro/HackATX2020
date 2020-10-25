bb373e7093c31ca354a3b652cf7bd71c9619c431
chitrang95 <chitrang1995@gmail.com>
2020-10-24 20:47:08 -0500

    This update puts google maps under the search bar on the first page of our website
    Google map loaded on bottom of the page. Changes are included in inded.php as well as master.css

--- shoplocal/js/firebase.js
+++ shoplocal/js/firebase.js
@@ -20,7 +20,21 @@
     </script>
 
     <script>
-      // The JavaScript code that creates the Firebase map goes here.
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 0, lng: 0},
+          zoom: 3,
+          styles: [{
+            featureType: 'poi',
+            stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+          }, {
+            featureType: 'transit.station',
+            stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+          }],
+          disableDoubleClickZoom: true,
+          streetViewControl: false
+        });
+      } 
     </script>
 
   </body>
--- shoplocal/php/index.php
+++ shoplocal/php/index.php
@@ -15,7 +15,7 @@
     </div>
 
     <div class="search-container">
-       <form action="./action.php">
+       <form>
          <div class="search-field">
            <input type="text" placeholder="Find..." name="find">
            <input type="text" placeholder="Near..." name="near">
@@ -23,6 +23,29 @@
          </div>
        </form>
     </div>
+    <div id="map"></div>
+    <script defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClkGyBgCWivGSoFt4iPkiU9ArZJJRKJAI
+        &libraries=visualization&callback=initMap">
+    </script>
+
+    <script>
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 0, lng: 0},
+        zoom: 3,
+        styles: [{
+          featureType: 'poi',
+          stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+        }, {
+          featureType: 'transit.station',
+          stylers: [{ visibility: 'off' }]  // Turn off bus stations, train stations, etc.
+        }],
+        disableDoubleClickZoom: false,
+        streetViewControl: false
+      });
+    }
+    </script>
 
     <!-- The core Firebase JS SDK is always required and must be listed first -->
     <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
